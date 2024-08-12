161d301f9a5405844ba995d61e7898833500385e
ATTRM33 <ATTRM33@gmail.com>
2024-08-11 21:28:11 -0500

    main program update

--- Main.cpp
+++ Main.cpp
@@ -8,36 +8,16 @@
 #include "Picture.h"
 
 int main() {
+
+    //problem 1.
     try {
-        std::string filename = "colors.txt";
-        std::ifstream file(filename);
-
-        if (!file.is_open()) {
-            throw ColorExceptions::fileNotFound(filename);
-        }
-
-        int width, height;
-        if (!(file >> width >> height)) {
-            throw ColorExceptions::colorFileFormat("Width and height not found at the beginning of the file.");
-        }
-
-        int x = -5, y = 10;
-        if (x < 0 || y < 0) {
-            throw ColorExceptions::coordinateOutOfRange(x, y);
-        }
-
-        int r = 300, g = 50, b = -20;
-
-        if (r < 0 || r > 255) {
-            ColorExceptions ex = ColorExceptions::colorOutOfRange(x, y, "Red", r);
-            std::cout << ex.getMessage() << std::endl;
-            r = std::max(0, std::min(255, r));
-        }
-
+        Picture pic("colors.txt");
+        std::cout << pic;
 
     }
     catch (const ColorExceptions& e) {
         std::cout << e.getMessage() << std::endl;
+        
     }
 
 }
